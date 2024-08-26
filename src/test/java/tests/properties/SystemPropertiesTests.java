2638661eb64b75e35e7df9bfee583db3f1d7da3b
Anth0nySt <s.anthony2808@gmail.com>
2024-08-26 11:32:17 +0300

    added attachments

---
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,13 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+
+    @Tag("Browser Change")
+    @Test
+    void systemPropertiesTest() {
+        String browser = System.getProperty("browser");
+    }
+}

7e3f34fee998d857d680f6b3471aeaf172416032
Anth0nySt <s.anthony2808@gmail.com>
2024-08-26 14:40:15 +0300

    added Properties

--- src/test/java/tests/properties/SystemPropertiesTests.java
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -1,13 +1,28 @@
 package tests.properties;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 public class SystemPropertiesTests {
 
     @Test
     @Tag("browser")
     void systemPropertiesTest() {
+        String browser = System.getProperty("browser","chrome");
+        System.out.println(browser);
+    }
+
+    @Test
+    @Tag("resolution")
+    void systemPropertiesTest2() {
+        Configuration.browserSize = "2560 x 1440";
+        String resolution = System.setProperty("browserSize","2560 x 1440");
+        System.out.println(resolution);
+    }
+
+    @Test
+    @Tag("browserVer")
+    void systemPropertiesTest3() {
         String browser = System.getProperty("browser","chrome");
         System.out.println(browser);
     }

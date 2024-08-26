880e18b9b673038ee854c5e0118abab136d11aa6
Anth0nySt <s.anthony2808@gmail.com>
2024-08-26 13:36:15 +0300

    added Properties

--- src/test/java/tests/JenkinsTest.java
+++ src/test/java/tests/JenkinsTest.java
@@ -22,6 +22,7 @@ public class JenkinsTest {
         Configuration.browserSize = "3840 x 2160";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,

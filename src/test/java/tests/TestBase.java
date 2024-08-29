d633c01e7d6012b96e6666547cf92806c81a5fa5
Anth0nySt <s.anthony2808@gmail.com>
2024-08-29 21:18:45 +0300

    added Properties

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,10 +12,10 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("version", "100.0");
         Configuration.browserSize = System.setProperty("resolution", "3840x2160");
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
 
